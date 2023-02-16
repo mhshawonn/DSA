c6ac94c61b35b35a47397248b0cd1b64175413c5
Mh Shawon <103067260+mhshawonn@users.noreply.github.com>
2023-02-17 00:26:51 +0600

    Add files via upload

---
+++ stack.cpp
@@ -0,0 +1,88 @@
+#include<bits/stdc++.h>
+using namespace std;
+class node{
+        public:
+        int data;
+        node *next;
+        node(int val){
+            data=val;
+            next=NULL;
+        }
+};
+class Stack{
+    node* top;
+    public:
+    Stack(){
+        top=NULL;
+    }
+    void push(int data){
+        node* temp=new node(data);
+        if(!temp){
+            cout<<"Stack overflow\n";
+            exit(1);
+        }
+        temp->data=data;
+        temp->next=top;
+        top=temp;
+    }
+    bool empty(){
+        return top==NULL;
+    }
+    int peek(){
+        if(!empty()){
+            return top->data;
+        }
+        else{
+            exit(1);
+        }
+    }
+    void pop(){
+        node* temp;
+        if(top==NULL){
+            cout<<"Stack underfollow\n";
+            exit(1);
+        }
+        else{
+            temp=top;
+            top=top->next;
+            free(temp);
+        }
+    }
+    void prints(){
+        node* temp;
+        if(top==NULL){
+            cout<<"Stack underfollow\n";
+        }
+        else{
+            temp=top;
+            while(temp!=NULL){
+                cout<<temp->data;
+                temp=temp->next;
+                if(temp!=NULL){
+                    cout<<"->";
+                }
+            }
+        }
+        cout<<endl;
+    }
+};
+int main(){
+    Stack s;
+    char a;
+    while(scanf("%c",&a)!=EOF){
+        if(a=='(' || a=='{' || a=='['){
+            s.push(a);
+        }
+        if(a==')' || a=='}' || a==']'){
+            s.pop();
+        }
+    }
+    if(!s.empty()){
+        cout<<"imbalance\n";
+    }
+    else{
+        cout<<"balance\n";
+    }
+    
+   return 0;
+}
